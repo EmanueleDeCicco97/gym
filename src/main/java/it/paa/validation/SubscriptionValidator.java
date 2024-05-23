29b36bccf3453147aba62b5e23b9713911e44e3f
Manuel <emanueledecicco97@gmail.com>
2024-05-23 12:33:16 +0200

    commenti su subcriptionValidator

--- src/main/java/it/paa/validation/SubscriptionValidator.java
+++ src/main/java/it/paa/validation/SubscriptionValidator.java
@@ -15,25 +15,33 @@ public class SubscriptionValidator implements ConstraintValidator<ValidSubscript
 
     @Override
     public boolean isValid(Customer customer, ConstraintValidatorContext context) {
+        // inizializzo la variabile 'valid' a true. sarà usata per tracciare la validità del cliente.
         boolean valid = true;
+        // ottengo l'abbonamento attivo del cliente.
         String subscription = customer.getActiveSubscription();
+        // ottengo la data di nascita del cliente.
         LocalDate birthDate = customer.getDateOfBirth();
 
+        // se il cliente non ha un abbonamento attivo, ritorno true (valido di default).
         if (subscription == null) {
             return valid;
         }
 
         if (!allowedSubscriptions.contains(subscription)) {
             valid = false;
+            // disabilito la validazione predefinita.
             context.disableDefaultConstraintViolation();
+            // creo una nuova violazione di validazione.
             context.buildConstraintViolationWithTemplate(
                             "Subscription must be one of the following: daily, monthly, yearly")
                     .addPropertyNode("activeSubscription")
                     .addConstraintViolation();
         }
-
+        // se la data di nascita non è null, controllo l'età del cliente.
         if (birthDate != null) {
+            // ottengo l'età del cliente.'
             int age = Period.between(birthDate, LocalDate.now()).getYears();
+            // controllo se l'età del cliente è minore di 14 o maggiore di 65.'
             if (age < 14) {
                 valid = false;
                 context.disableDefaultConstraintViolation();
@@ -41,6 +49,8 @@ public class SubscriptionValidator implements ConstraintValidator<ValidSubscript
                                 "Customers must be at least 14 years old to subscribe")
                         .addPropertyNode("dateOfBirth")
                         .addConstraintViolation();
+                // se l'abbonamento è annuale e l'età è superiore a 65 anni, imposto 'valid' a false e costruisco un messaggio di violazione
+                // della costrizione.
             } else if ("yearly".equals(subscription) && age > 65) {
                 valid = false;
                 context.disableDefaultConstraintViolation();
@@ -50,7 +60,7 @@ public class SubscriptionValidator implements ConstraintValidator<ValidSubscript
                         .addConstraintViolation();
             }
         }
-
+        // ritorno il valore di validità del cliente.
         return valid;
     }
 }
