001d158e309e594e55d1fb3eade3030ebcad2ba9
Manuel <emanueledecicco97@gmail.com>
2024-05-28 13:53:42 +0200

    commenti su validazione

--- src/main/java/it/paa/validation/ValidIntensity.java
+++ src/main/java/it/paa/validation/ValidIntensity.java
@@ -5,10 +5,16 @@ import jakarta.validation.Payload;
 
 import java.lang.annotation.*;
 
+// Indica che questa annotazione dovrebbe essere inclusa nella documentazione generata automaticamente.
 @Documented
+// Specifica la classe che implementa la logica di validazione per questa annotazione.
 @Constraint(validatedBy = IntensityValidatorCheck.class)
+// Indica che questa annotazione può essere applicata a qualsiasi tipo di elemento.
 @Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER, ElementType.ANNOTATION_TYPE})
+//Indica che questa annotazione deve essere conservata e resa disponibile in fase di runtime, in modo che
+//possa essere utilizzata per la convalida dei dati a runtime.
 @Retention(RetentionPolicy.RUNTIME)
+
 public @interface ValidIntensity {
     String message() default "Invalid intensity value. Must be 'easy', 'medium', or 'hard'.";
 
